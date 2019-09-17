238d2002cd29ecd5c5ab4e3afd7d6282f7fe212b
CamilaEVR <55298469+CamilaEVR@users.noreply.github.com>
2019-09-17 17:26:42 -0500

    Update and rename ejercicio2/main.cpp to  ejercicio2/main.cpp

--- ejercicio2/main.cpp
+++ ejercicio2/main.cpp
@@ -1,11 +1,50 @@
 #include <iostream>
-#include "Tipos.h"
-using namespace std;
+#include <string>
+
+using std::cout;
+using std::cin;
+using std::endl;
+int tipoEntrada;
+int precioEntrada;
+char esCliente;
 
 int main() {
-
-
-// ToDO
-
-  return 0;
+	cout << R"(
+	---MENU---
+	Tipo de entrada
+	1. Super Vip	=> S/.212
+	2. Vip		=> S/.170
+	3. Preferencial => S/.136
+	4. General	=> S/.59
+)";
+	do {
+		cout << "Seleccione el tipo de entrada que desea (numero del 1 al 4): ";
+		cin >> tipoEntrada;
+	} while (tipoEntrada > 4 || tipoEntrada < 1);
+	switch (tipoEntrada) {
+	case 1:
+		precioEntrada = 212;
+		break;
+	case 2:
+		precioEntrada = 170;
+		break;
+	case 3:
+		precioEntrada = 136;
+		break;
+	case 4:
+		precioEntrada = 59;
+		break;
+	}
+	do {
+		cout << "¿Es usted cliente actual de Claro? Por favor ingrese S o N: ";
+		cin >> esCliente;
+	} while (esCliente != 'S' && esCliente != 's' && esCliente != 'n' && esCliente != 'N');
+	switch (esCliente) {
+	case 's':
+	case 'S':
+		precioEntrada = precioEntrada - (precioEntrada * 0.20);
+	default:
+		precioEntrada = precioEntrada;
+	}
+	cout << "El monto a pagar es: " << precioEntrada << endl;
 }
